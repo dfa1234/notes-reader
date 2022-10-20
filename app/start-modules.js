64c9832737d9429c7b06c463bbd371e64875a891
David Faure <David.Faure@sbdinc.com>
2022-10-20 18:33:57 +0300

    reorder index

--- app/start-modules.js
+++ app/start-modules.js
@@ -1,11 +1,12 @@
 import {Piano} from './piano.js';
 import {Audio} from './audio.js';
 
+const piano = new Piano();
+piano.render();
+
 document.getElementById('allow-microphone-capture').addEventListener('click', () => {
     document.getElementById('app-wrapper').style.display = 'block';
-
-    const piano = new Piano();
-    piano.render();
+    document.getElementById('allow-microphone-capture').style.display = 'none';
 
     const audio = new Audio();
     audio.start();
